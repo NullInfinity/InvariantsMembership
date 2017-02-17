901c4b378d721224bd8daa2d00d3f516e747c8a4
Alex Thorne <alex@alexthorne.net>
2017-02-17 14:23:07 +0000

    Fix barcode entry callback

--- invar.py
+++ invar.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python3
 
 # quick and dirty import :)
+import sqlite3
 from tkinter import *
 from tkinter import ttk
 
-APP_NAME = 'Invariants Membership System'
+APP_NAME = 'Invariants Membership'
+BARCODE_LENGTH = 7
 
+TEXT_ACCEPTED = 'MEMBER'
+TEXT_REJECTED = 'NOTMEMBER'
 
-def checkmember(sv, *args):
-    print(sv)
-    print(type(sv))
-    print(args)
-    for arg in args:
-        print(type(arg))
+def querydb(barcode=None, firstname=None, lastname=None):
+    return False
+
+def checkmember(sv):
+    barcode = sv.get()
+
+    if len(barcode) < BARCODE_LENGTH:
+        return
+
+    barcode = barcode[:BARCODE_LENGTH]
+
+    if querydb(barcode=barcode):
+        result_label.config(text=TEXT_ACCEPTED)
+        add_button.config(state=DISABLED)
+    else:
+        result_label.config(text=TEXT_REJECTED)
+        add_button.config(state=NORMAL)
 
 
 class TextBox:
@@ -27,8 +42,8 @@ class TextBox:
         self.widget.grid(column=2, row=row, sticky=(W, E))
 
         # text entered callback
-        if callback:
-            self.value.trace('w', callback=callback)
+        if callback is not None:
+            self.value.trace('w', callback=lambda name, index, mode, var=self.value: callback(var))
 
 # make app
 root = Tk()
@@ -43,11 +58,12 @@ barcode = TextBox('Barcode', row=1, callback=checkmember)
 firstname = TextBox('First Name', row=2)
 lastname = TextBox('Last Name', row=3)
 
-result = StringVar()
-label = ttk.Label(textvariable=result)
-label.grid(column=0, row=4, sticky=(W, E))
-button = ttk.Button(text='Add')
-button.grid(column=0, row=5, sticky=(W, E))
+result_var = StringVar()
+result_label = ttk.Label(text='test')
+result_label.grid(column=0, row=4, columnspan=4, sticky=(W, E))
+add_button = ttk.Button(text='Add')
+add_button.grid(column=0, row=5, sticky=(W, E))
+add_button.config(state=DISABLED)
 
 for child in frame.winfo_children():
     child.grid_configure(padx=5, pady=5)
